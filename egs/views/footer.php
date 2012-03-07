aeae169ba80fde730ac0f208ab77e648d4cb418b
Joseph Newing <jnewing@gmail.com>
2012-03-07 11:37:23 -0500

    old bg removal, added copyright notice

--- egs/views/footer.php
+++ egs/views/footer.php
@@ -1,3 +1,9 @@
+		<!-- 
+			// Copyright
+			// Please leave this alone, I'm allowing you to use EGs FREE, the leaset
+			// you can do it leave this one copyright tag alone.
+			// kthx - synmuffin
+		-->
 		<footer id="copyright"><a href="irc://pool.ircmojo.org/egs">EGs Web Panel</a> - (Copyright &copy; <a href="http://epicgeeks.net/egs/">synmuffin</a>)</footer>
 	</div>
 </body>
